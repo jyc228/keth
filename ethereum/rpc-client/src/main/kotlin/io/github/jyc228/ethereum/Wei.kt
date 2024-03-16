f32f9e388f63269cbd44fc4f560bf58e5682f896
Logan <logan@lightscale.io>
2024-03-16 17:13:12 +0900

    refac: refac wei

--- ethereum/rpc-client/src/main/kotlin/io/github/jyc228/ethereum/Wei.kt
+++ ethereum/rpc-client/src/main/kotlin/io/github/jyc228/ethereum/Wei.kt
@@ -4,7 +4,14 @@ import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.SerializerProvider
 import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import java.lang.ref.WeakReference
 import java.math.BigInteger
+import java.util.WeakHashMap
+
+val Int.eth get() = wei(this) * Wei.ONE_ETH
+val Int.wei get() = wei(this)
+val Long.wei get() = wei(this)
+val BigInteger.wei get() = wei(this)
 
 @JvmInline
 @JsonSerialize(using = Serializer::class)
@@ -26,11 +33,6 @@ value class Wei(val v: BigInteger) {
     companion object {
         val ONE_GWEI = Wei(1000000000.toBigInteger())
         val ONE_ETH = Wei(1000000000000000000.toBigInteger())
-
-        val Int.eth get() = Wei(this.toBigInteger()) * ONE_ETH
-        val Int.wei get() = Wei(this.toBigInteger())
-        val Long.wei get() = Wei(this.toBigInteger())
-        val BigInteger.wei get() = Wei(this)
     }
 }
 
@@ -38,4 +40,10 @@ private class Serializer : JsonSerializer<Wei>() {
     override fun serialize(value: Wei, gen: JsonGenerator, serializers: SerializerProvider) {
         gen.writeString(value.v.toString())
     }
+}
+
+private val cache = WeakHashMap<String, WeakReference<Wei>>()
+private fun wei(v: Any): Wei {
+    val stringV = v.toString()
+    while (true) return cache.getOrPut(stringV) { WeakReference(Wei(stringV.toBigInteger())) }.get() ?: continue
 }
