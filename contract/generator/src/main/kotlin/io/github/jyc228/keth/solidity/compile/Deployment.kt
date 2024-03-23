b1ed866c0999a3c4d51b80f7d0fa1e9a72653704
Logan <logan@lightscale.io>
2024-03-23 20:34:33 +0900

    feat: add solc metadata data model

--- contract/generator/src/main/kotlin/io/github/jyc228/keth/solidity/compile/Deployment.kt
+++ contract/generator/src/main/kotlin/io/github/jyc228/keth/solidity/compile/Deployment.kt
@@ -1,4 +1,4 @@
-package io.github.jyc228.keth.solidity
+package io.github.jyc228.keth.solidity.compile
 
 import io.github.jyc228.solidity.AbiItem
 import kotlinx.serialization.Serializable
@@ -24,37 +24,6 @@ class Deployment(
 ) {
     val metadataObject: Metadata = Json { ignoreUnknownKeys = true }.decodeFromString(metadata)
 
-    @Serializable
-    data class Metadata(
-        val compiler: Compiler,
-        val language: String,
-        val output: Output,
-        val settings: Settings,
-        val sources: Map<String, JsonObject>,
-        val version: Int
-    )
-
-    @Serializable
-    data class Compiler(
-        val version: String
-    )
-
-    @Serializable
-    data class Output(
-        val abi: List<AbiItem>,
-        val devdoc: Doc,
-        val userdoc: Doc
-    )
-
-    @Serializable
-    data class Settings(
-        val compilationTarget: Map<String, String>,
-        val evmVersion: String,
-        val libraries: JsonObject,
-        val metadata: JsonObject,
-        val optimizer: JsonObject,
-        val remappings: List<String>
-    )
 
     @Serializable
     data class StorageLayout(
@@ -70,13 +39,5 @@ class Deployment(
         val offset: Int,
         val slot: String,
         val type: String
-    )
-
-    @Serializable
-    data class Doc(
-        val kind: String = "",
-        val methods: Map<String, JsonObject> = emptyMap(),
-        val version: Int = 0,
-        val notice: String = "",
     )
 }
