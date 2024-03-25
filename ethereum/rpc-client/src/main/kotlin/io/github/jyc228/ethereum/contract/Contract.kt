0bd8ef22ffb699a97e71e959d16e4596c85a5829
Logan <logan@lightscale.io>
2024-03-25 15:12:16 +0900

    fix: implement Factory.encodeParameters

--- ethereum/rpc-client/src/main/kotlin/io/github/jyc228/ethereum/contract/Contract.kt
+++ ethereum/rpc-client/src/main/kotlin/io/github/jyc228/ethereum/contract/Contract.kt
@@ -1,10 +1,14 @@
 package io.github.jyc228.ethereum.contract
 
 import io.github.jyc228.ethereum.Address
+import io.github.jyc228.ethereum.abi.Abi
 import io.github.jyc228.ethereum.rpc.ApiResult
 import io.github.jyc228.ethereum.rpc.eth.EthApi
 import io.github.jyc228.ethereum.rpc.eth.GetLogsRequest
 import io.github.jyc228.ethereum.rpc.eth.Log
+import io.github.jyc228.solidity.AbiItem
+import kotlinx.serialization.json.Json
+import org.intellij.lang.annotations.Language
 
 interface Contract<EVENT : ContractEvent> {
     suspend fun getLogs(options: (GetLogsRequest.() -> Unit)? = null): ApiResult<List<Pair<EVENT, Log>>>
@@ -14,7 +18,12 @@ interface Contract<EVENT : ContractEvent> {
         filterParameter: (GetLogsRequest.(indexedParam: INDEXED) -> Unit)? = null
     ): ApiResult<List<Pair<EVENT, Log>>>
 
-    abstract class Factory<T : Contract<*>>(val create: (Address, EthApi) -> T)
+    abstract class Factory<T : Contract<*>>(val create: (Address, EthApi) -> T) {
+        protected fun encodeParameters(@Language("json") jsonAbi: String, vararg args: Any?): String {
+            val abi: AbiItem = Json.decodeFromString(jsonAbi)
+            return Abi.encodeParameters(abi.inputs.map { it.type }, args.toList())
+        }
+    }
 }
 
 interface ContractEvent
