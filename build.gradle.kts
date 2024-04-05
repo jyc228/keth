2699a5c23be8459f72352806459b412a2b38e4d9
Logan <logan@lightscale.io>
2024-04-05 11:39:26 +0900

    feat: implement kotlin abi codec

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,10 @@ allprojects {
 
     dependencies {
         testImplementation(kotlin("test"))
-        testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
-        testImplementation("io.kotest:kotest-runner-junit5:5.6")
-        testImplementation("io.kotest:kotest-assertions-core:5.6")
+        testImplementation("io.kotest:kotest-runner-junit5:5.8.1")
+        testImplementation("io.kotest:kotest-runner-junit5:5.8.1")
+        testImplementation("io.kotest:kotest-assertions-core:5.8.1")
+        testImplementation("io.kotest:kotest-framework-datatest:5.8.1")
     }
 
     kotlin {
