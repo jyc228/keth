e2f0a71135c97351e5ed468c85f8ea9754bc26cc
Logan <logan@lightscale.io>
2024-04-10 13:33:41 +0900

    feat: publish rlp library

--- rlp/build.gradle.kts
+++ rlp/build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     kotlin("jvm")
+    `maven-publish`
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
+
+publishing(createGPRPublisher { artifactId = "rlp" })
